a2a2f708e148cfc43b0611a916b7f6b7117f165f
mark <you@example.com>
2023-08-23 11:13:21 -0500

    init

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,36 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- enable clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- use spaces for tabs and whatnot
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- buffers
+vim.keymap.set('n', '<S-l>', ':bnext<cr>')
+vim.keymap.set('n', '<S-h>', ':bprev<cr>')
+vim.keymap.set('n', '<Leader>x', ':bdelete<cr>')
+
+-- Rebind Ctrl + H to move left between splits
+vim.api.nvim_set_keymap('n', '<C-H>', '<C-W>h', { noremap = true, silent = true })
+-- Rebind Ctrl + L to move right between splits
+vim.api.nvim_set_keymap('n', '<C-L>', '<C-W>l', { noremap = true, silent = true })
+
+
+
+
+
